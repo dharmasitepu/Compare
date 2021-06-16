69eaec808329c64fb4440832a3fb5e24ae82a114
Dharma <64702275+dharmasitepu@users.noreply.github.com>
2021-06-16 21:54:02 +0700

    Update compare.c

--- compare.c
+++ compare.c
@@ -4,317 +4,323 @@
 
 FILE *data;
 FILE *isi_data;
-int ada=0;
-void adminmenu();void registrasi(); void login(); void usermenu(); void daftarpasien(); void pilihjadwal();void login();
-void loginpasien();void loginadmin();void inputjadwal();void inputvaksin(); void find(); void formulir();
-int loginadminmenu, n, i, p, luser;
-bool ceknampes(char nampes[100]);
-struct {
-	int kuotavaksin;
-	char nama[100], username[100],pass[100],tanggal[100], jenisvaksin[100], lok[100], user[100], pw[100];
-	char usernamea[100],passa[50], cari1[100], cari2[100], status[100], jadwal[100];
-	
-	}logina, a, daftar, temp[100], temp2;
+FILE *form;
+void cover();
+void adminlogin();
+void adminmenu();
+void inputjadwal();
+void lihatpasien();
+void registrasi();
+void loginpasien();
+void menupasien();
+void lihatjadwal();
+void find();
+void isiform();
+int dota,dotb,dotc,i,n,a;
+char plogin[50],ulogin[50],c[50],tempvv[50],jenvv[50];
 struct{
-	char tempat[100], jenis[100], name[100], umur[100], alamat[100], nik[100], gender[100];
-}datay;	
-void main()
-{
-	ulang:
-	system("cls");
-	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	puts("1. Login Admin");
-	puts("2. Registrasi");
-	puts("3. Login Pasien ");
-	puts("4. Exit");
-	printf("\n\nPilih Menu : "); scanf("%d",&p);
-	fflush(stdin);
-	switch(p)
-	{
-	case 1:
-		loginadmin();
-		break;
-	case 2:
-		registrasi();
-		break;
-	case 3:
-		loginpasien();
-		break;
-	case 4:
-		printf("Terima kasih, Selamat Datang Kembali\n");		
-		break;	
-	}
+    char tempat[50],tanggal[50],jenisvaksin[50],user[50],pass[50];
+    char userb[50],passb[50],userp[50],passp[50];
+    int kuota;
+} jadwal,login,loginb,p;
+
+struct{
+    char nama[50],jenisk[50],umur[50],alamat[50],noktp[50],tempv[50],jenv[50];
+}datap;
+
+void cover(){
+   printf("\n\t\t DDDDDDDD   '''   VVV       VVV   AAAAAAAA    KKK   KKK      SSSSSSSSSS    III  NNN     NNN");
+ printf("\n\t\t DDDDDDDDD  '''   VVV       VVV  AAA    AAA   KKK  KKK     SSSSSS   SSSSS  III  NNNN    NNN");
+ printf("\n\t\t DDD    DDD '''   VVV       VVV  AAA    AAA   KKK KKK       SSSSS     SSS  III  NNNN    NNN");
+ printf("\n\t\t DDD    DDD '''   VVV       VVV  AAAAAAVAAA   KKKK            SSSSS        III  NNN N   NNN");
+ printf("\n\t\t DDD    DDD        VVV     VVV   AAAAAAVAAA   KKKK               SSSSS     III  NNN  N  NNN");
+ printf("\n\t\t DDD    DDD         VVV   VVV    AAA    AAA   KKK KKK      SSS     SSSSS   III  NNN   N NNN");
+ printf("\n\t\t DDDDDDDDD           VVV VVV     AAA    AAA   KKK  KKK      SSSS   SSSSSS  III  NNN    NNNN");
+ printf("\n\t\t DDDDDDDD              VVVV      AAA    AAA   KKK    KKK     SSSSSSSSSS    III  NNN     NNN");
+ printf("\n\t\t=============================================================================================\n");
+
+ printf("\n\n");
 }
 
-void loginadmin()
-{
-	system("cls");
-			puts("\t\t\t================================");
-			puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-			puts("\t\t\t================================");
-			puts("\t\t\t\t Login Admin\n"); 
-			printf("username : ");fflush(stdin);gets(logina.usernamea);
-			printf("password : ");fflush(stdin);gets(logina.passa);
-				if(strcmp(logina.usernamea,"admin")==0&&strcmp(logina.passa,"admin")==0)
-				{
-					system("cls");adminmenu();
-				}
-				else{
-					system("cls");
-				}
-		}
-	
-void adminmenu(){
-	system ("cls");
-	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	puts("\t\t\t\t Selamat Datang Admin\n"); 
-	puts("1. Input Jadwal");
-	puts("2. Daftar Pasien");
-	puts("3. Exit");
-	printf("Pilihan : "); scanf("%d",&loginadminmenu);
-	switch(loginadminmenu)
-	{
-	case 1:{
-		inputjadwal();
-		break;
-	}
-	case 2:{
-		daftarpasien();
-		break;
-	}
-	case 3:{
-		main();
-		break;
-	}
-	default :printf ("Error!");
-}
-}
-
-void inputjadwal(){
-	system ("cls");
-	char tanggal[100], jenisvaksin[100], lok[100];
-	int kuotavaksin, n, g;
-	FILE*jadwal;
-	jadwal=fopen("List tanggal.dat","ab+");
-	printf("Banyak jadwal : "); scanf("%d",&g); getchar();
-	for(i=1;i<=g;i++){
-	printf("%d. Tanggal\t: ",i);gets(a.tanggal); fflush(stdin);
-	printf("Jenis Vaksin\t: ");gets(a.jenisvaksin); fflush(stdin);
-	printf("Tempat\t\t: ");gets(a.lok); fflush(stdin);
-	printf("Kuota\t\t: ");scanf("%d", &a.kuotavaksin); fflush(stdin);
-	fwrite(&a, sizeof (a), 1, jadwal);
-	}fclose(jadwal); 
-	system("pause");
-	adminmenu();
-}
-
-void daftarpasien(){
-	system ("cls");
-	int i=1,n;
-	FILE*list; 
-	list=fopen("akun.dat","rb");
-	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	printf("Daftar Pasien:\n");
-	while(fread(&daftar,sizeof(daftar),1,list)==1){
-		printf("%d. %s\n",i,daftar.nama);
-		i++;
-		}
-	fclose(list); 
-	system("pause");adminmenu();
-}
-
-void registrasi(){
-	FILE *regis;
-    system("cls");
- 	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	printf("\t\t\t\t Silahkan Regis \n");
-    regis=fopen("akun.dat","ab");
- {
-	printf("nama\t\t: ");fflush(stdin);
-    gets(daftar.nama);
-	printf("username\t: ");fflush(stdin);
-    gets(daftar.username);
-  	printf("password\t: ");
-    gets(daftar.pass);
-    fwrite(&daftar, sizeof(daftar),1, regis);
- }
-    fclose(regis); 
-	printf("REGISTRASI BERHASIL\n");system("pause");
-    main();
-}
-
-void loginpasien(){
-	char user[100], pw[100];
-	FILE *in;
-    system("cls");
-    puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	printf("\t\t\t\t Silahkan Login \n");
- 	printf("Username : ");fflush(stdin); gets(user);
-    printf("\nPassword : ");fflush(stdin); gets(pw);
-    in=fopen("akun.dat","rb");
-    while(fread(&daftar,sizeof(daftar),1,in)==1)
+void main(){
+    system("cls");cover();
+    puts("1. Login Admin");
+    puts("2. Login Pasien");
+    puts("3. Registrasi Pasien");
+    puts("4. Exit");
+    printf("\n\nPilih Menu : ");
+    scanf("%d",&dota);
+    switch(dota)
     {
-        if((strcmp(daftar.username,user)==0 && strcmp(daftar.pass,pw)==0)){
-            printf("LOGIN BERHASIL\n");
-            fclose(in);system("pause");usermenu();
+    case 1:
+        adminlogin();
+        break;
+    case 2:
+        loginpasien();
+        break;
+    case 3:
+        registrasi();
+        break;
+    case 4:
+        printf("\nTerima kasih, Selamat Datang Kembali\n");
+        break;
+    }
+}
+void adminlogin(){
+    system("cls");cover();
+    puts("\t\t\t\t Login Admin\n");
+    printf("username : ");
+    fflush(stdin);
+    gets(login.user);
+    printf("password : ");
+    fflush(stdin);
+    gets(login.pass);
+    if(strcmp(login.user,"admin")==0&&strcmp(login.pass,"admin")==0)
+    {
+        puts("---Login Berhasil---");
+        system("pause");
+        adminmenu();
+    }
+    else
+    {
+        puts("--Username atau Password salah--");
+        system("pause");
+        main();
+    }
+
+}
+void adminmenu(){
+    system("cls");cover();
+    puts("==Hello Admin==");
+    puts("1. Input Jadwal Vaksin");
+    puts("2. Lihat Pasien Vaksin");
+    puts("3. Kembali");
+    printf("\nPilihan : ");
+    scanf("%d",&dotb);
+    switch(dotb)
+    {
+    case 1:{
+        inputjadwal();
+        break;
+    }
+    case 2:{
+        lihatpasien();
+        break;
+    }
+    case 3:{
+        main();
+        break;
+    }
+    default :
+        printf("ERROR!!");
+    }
+}
+void inputjadwal(){
+    system("cls");cover();
+    FILE *data;
+    puts("\t\t\t<===List Jadwal Vaksinasi===>");
+    puts("Tanggal\t\t\tTempat\t\tKuota\t\tJenis Vaksin");
+    data=fopen("jadwalvaksin.dat","rb");
+    while(fread(&jadwal,sizeof(jadwal),1,data)==1){
+    printf("\n%s", jadwal.tanggal);
+    printf("\t\t%s", jadwal.tempat);
+    printf("\t\t%d", jadwal.kuota);
+    printf("\t\t%s", jadwal.jenisvaksin);
+    }
+    fclose(data);
+    data=fopen("jadwalvaksin.dat","ab");
+    printf("\nInput Tanggal : ");fflush(stdin);
+    gets(jadwal.tanggal);
+    printf("Input Tempat  : ");
+    gets(jadwal.tempat);
+    printf("Kuota Vaksin : ");fflush(stdin);
+    scanf("%d", &jadwal.kuota);
+    printf("Jenis Vaksin : ");fflush(stdin);
+    gets(jadwal.jenisvaksin);
+    fwrite(&jadwal, sizeof(jadwal),1, data);
+    fclose(data);
+    adminmenu();
+
+}
+void lihatpasien(){
+    system("cls");cover();
+    form=fopen("formulir.dat","rb");
+    while(fread(&datap,sizeof(datap),1,form)==1){
+            puts("============================");
+            printf("\nNama          : %s",datap.nama);
+            printf("\nJenis Kelamin : %s",datap.jenisk);
+            printf("\nUmur          : %s",datap.umur);
+            printf("\nNo.KTP        : %s",datap.noktp);
+            printf("\nAlamat        : %s",datap.alamat);
+            printf("\nTempat Vaksin : %s",datap.tempv);
+            printf("\nJenis Vaksin  : %s\n",datap.jenv);
+            puts("============================");
+    }
+    fclose(form);
+    system("pause");
+    adminmenu();
+}
+void registrasi(){
+    system("cls");cover();
+    puts("---Silahkan Registrasi---");
+ isi_data=fopen("Akun pasien.dat","ab");
+
+ {
+  printf("username\t: ");fflush(stdin);
+        gets(loginb.userb);
+  printf("password\t: ");fflush(stdin);
+        gets(loginb.passb);
+  fwrite(&loginb, sizeof(loginb),1, isi_data);
+ }
+ fclose(isi_data);
+ main();
+}
+void loginpasien(){
+    system("cls");cover();
+    puts("---Silahkan Masuk---");
+	printf("Username : ");fflush(stdin); gets(ulogin);
+    printf("\nPassword : "); gets(plogin);
+    isi_data=fopen("Akun pasien.dat","rb");
+    while(fread(&loginb,sizeof(loginb),1,isi_data)==1)
+    {
+        if((strcmp(loginb.userb,ulogin)==0 && strcmp(loginb.passb,plogin)==0)){
+            printf("----Login berhasil----\n");
+            fclose(isi_data);system("pause");menupasien();
         }
-    }rewind(in);
-    while(fread(&daftar,sizeof(daftar),1,in)==1){
-        if((strcmp(daftar.username,user)!=0 || strcmp(daftar.pass,pw)!=0)){
-            printf("LOGIN GAGAL\n");
-            fclose(in);system("pause");main();
+    }rewind(isi_data);
+    while(fread(&loginb,sizeof(loginb),1,isi_data)==1){
+        if((strcmp(loginb.userb,ulogin)!=0 || strcmp(loginb.passb,plogin)!=0)){
+            printf("----Login GAGAL----\n");
+            fclose(isi_data);system("pause");main();
         }
     }
-    fclose(in);
+    fclose(isi_data);
+    main();
 }
-		
-void usermenu(){
-	system("cls");
-	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	printf("\t\t\t\t SELAMAT DATANG \n");
-	puts("Menu : ");
-	puts("1. Sorting dan Cari Vaksin");
-	puts("2. Formulir Pendaftaran");
-	puts("3. Exit ");
-	printf("Pilihan : "); scanf("%d",&luser);
-	 switch(luser){
-	case 1:{
-		find();
-		break;
-	}
-	case 2:{
-		formulir();
-		break;
-	}
-	case 3:{
-		main();
-		break;
-		default :printf ("Error!");
-	}
+void menupasien(){
+    system("cls");cover();
+    puts("==Hello Mr or Mrs==");
+    puts("1. Lihat Jadwal Jenis Vaksin");
+    puts("2. Isi Formulir Pendaftaran");
+    puts("3. Kembali");
+    printf("\nPilihan : ");
+    scanf("%d",&dotc);
+    fflush(stdin);
+    switch(dotc)
+    {
+    case 1:{
+        lihatjadwal();
+        break;
+    }
+    case 2:{
+        isiform();
+        break;
+    }
+    case 3:{
+        main();
+        break;
+    }
+    }
+
 }
+void lihatjadwal(){
+    system("cls");cover();
+    puts("\t\t\t<===List Jadwal Vaksinasi===>");
+    puts("Tanggal\t\t\tTempat\t\tKuota\t\tJenis Vaksin");
+    data=fopen("jadwalvaksin.dat","rb");
+    while(fread(&jadwal,sizeof(jadwal),1,data)==1){
+        printf("%s", jadwal.tanggal);
+        printf("\t\t%s", jadwal.tempat);
+        printf("\t\t%d", jadwal.kuota);
+        printf("\t\t%s\n", jadwal.jenisvaksin);
+    }
+    fclose(data);
+    system("pause");
+    menupasien();
 }
 
-void find(){
-	FILE *sorting;
-	char tanggal[100], jenisvaksin[100], lok[100], cari[100];
-	int kuotavaksin, i=0, j=0, n=0;
-	sorting=fopen("List tanggal.dat","rb");
-	while(fread(&a, sizeof(a),1,sorting)==1){
-		strcpy(temp[i].tanggal, a.tanggal);
-		strcpy(temp[i].jenisvaksin, a.jenisvaksin);
-		strcpy(temp[i].lok, a.lok);
-		temp[i].kuotavaksin=a.kuotavaksin;
-		i++, n++;}
-	i=0; j=0;
-	for(i=n-1;i>=1;i--){
-		for(j=1;j<=i;j++){
-			if(temp[j-1].kuotavaksin<temp[j].kuotavaksin){
-			temp2=temp[j-1];
-			temp[j-1]=temp[j];
-			temp[j]=temp2;
-			}
-		}
-	}
-	
-	for (i=0;i<n;i++){
-		printf("%d.\tTanggal\t: %s\n", i+1,temp[i].tanggal);
-		printf("\tJenis Vaksin\t: %s\n", temp[i].jenisvaksin);
-		printf("\tKuota\t: %d\n", temp[i].kuotavaksin);
-		printf("\tTempat\t: %s\n",temp[i].lok);
-	}
-	fclose(sorting); getchar();
-	printf("Jenis vaksin yang dicari : "); gets(cari); system("cls");
-	i=0;
-	while(i<n && strcmp(temp[i].jenisvaksin,cari)!=0){ 
-		i++;
-	}
-	if(strcmp(cari,temp[i].jenisvaksin)==0){ 
-		printf("%d. Tanggal\t: %s\n",i+1,temp[i].tanggal); 
-		printf("   Jenis Vaksin\t: %s\n",temp[i].jenisvaksin); 
-		printf("   Tempat\t: %s\n", temp[i].lok); 
-		printf("   Kuota\t: %d\n",temp[i].kuotavaksin);
-		} else{
-		printf("Data tidak ditemukan");
-	}system("pause"); usermenu();
-}
 
-void formulir(){
-	char pil[3], tempat[100], jenis[100];
-	system ("cls");
-	int i=1,n;
-	FILE*form; 
-	FILE*formu;
-	form=fopen("List tanggal.dat","rb+");
-	formu=fopen("Formulir.dat","ab");
-	puts("\t\t\t================================");
-	puts("\t\t\t SELAMAT DATANG DI D'VAKSIN");
-	puts("\t\t\t================================");
-	printf("\nApakah Pasien Positif Covid atau Tidak? (Ya/No)? : ");scanf("%s",&pil);getchar();
-	system ("cls");
-    if(strcmp(pil,"NO")==0||strcmp(pil,"No")==0||strcmp(pil,"no")==0){
-	while(fread(&a,sizeof(a),1,form)==1){
-		printf("%d. Tanggal: %s\n",i,a.tanggal);
-		printf("	Tempat: %s\n",a.lok);
-		printf("	Jenis Vaksin: %s\n", a.jenisvaksin);
-		printf("	Kuota: %d\n", a.kuotavaksin);
-		i++;
-		}fclose(form);
-		printf(" Pilih Tempat: ");gets(datay.tempat);
-		printf(" Pilih Jenis Vaksin: ");gets(datay.jenis);
-		fwrite(&datay, sizeof(datay),1,formu);
-		fclose(formu);
-		formu=fopen("Formulir.dat","wb");
-		form=fopen("List tanggal.dat","rb+");
-		while(fread(&a,sizeof(a),1,form)==1){
-			if(strcmp(datay.tempat,a.lok)==0 && strcmp(datay.jenis,a.jenisvaksin)==0){
-				if(a.kuotavaksin>0){
-					ada=1;
-					fseek(form,-sizeof(a),SEEK_CUR);
-					a.kuotavaksin-= 1;
-					system("cls");
-		fwrite(&a,sizeof(a),1,form);
-						printf("Nama		 : "); gets(datay.name);
-    					printf("NIK			 : "); gets(datay.nik);
-    					printf("Alamat		 : "); gets(datay.alamat);
-    					printf("Umur		 : "); gets(datay.umur);
-    					printf("Jenis Kelamin: "); gets(datay.gender);
-    					fwrite(&datay,sizeof(datay),1,formu);
-						printf("Pendaftaran Berhasil");fflush(stdin);
-						fclose(formu);
-	system("cls");
-	formu=fopen("Formulir.dat","rb");
-	printf("\tBUKTI PENDFATRAN\n");
-	while(fread(&datay,sizeof(datay),1,formu)==1){
-    printf("Nama		: %s \n",datay.name);
-    printf("NIK  		: %s \n",datay.nik);
-    printf("Umur  		: %s \n",datay.umur);
-    printf("Gender  	: %s \n",datay.gender);
-    printf("Alamat 		: %s \n",datay.alamat);
-	printf("Lokasi 		: %s \n",datay.tempat);
-	printf("Jenis Vaksin: %s \n",datay.jenis);
-	}fclose(formu);
-				}
-				break;
-			}
-		}
-		fclose (form);
-		if(ada!=1){
-			printf("Data Tidak Ditemukan\n");
-		}
-		}else{
-	printf("Mohon Maaf, Anda Tidak Diperkenankan Melanjutkan Pendaftaran Karena Terjangkit COVID 19\n");
-	} system("pause"); usermenu();
-}
+void isiform(){
+    system("cls");cover();
+    FILE *form;
+    printf("Apakah anda terinveksi Virus Covid-19?(Y/N) : ");
+    gets(c);
+    if (strcmp(c,"N")==0 || strcmp(c,"n")==0) {
 
+    	puts("\n\t\t\t<===List Jadwal Vaksinasi===>");
+    	puts("Tanggal\t\t\tTempat\t\tKuota\t\tJenis Vaksin");
+    	data=fopen("jadwalvaksin.dat","rb");
+    	while(fread(&jadwal,sizeof(jadwal),1,data)==1){
+        printf("%s", jadwal.tanggal);
+        printf("\t\t%s", jadwal.tempat);
+        printf("\t\t%d", jadwal.kuota);
+        printf("\t\t%s\n", jadwal.jenisvaksin);
+    	}
+    	fclose(data);
+
+    	printf("\nPilih tempat Vaksin\t: "); gets(tempvv);
+    	printf("Pilih jenis Vaksin\t: "); gets(jenvv);
+
+    	 data=fopen("jadwalvaksin.dat","rb");
+        while(fread(&jadwal,sizeof(jadwal),1,data)==1)
+        {
+            if((strcmp(jadwal.tempat,tempvv)==0 && strcmp(jadwal.jenisvaksin,jenvv)==0))
+            {
+                if(jadwal.kuota>0){
+                    data=fopen("jadwalvaksin.dat","ab+");
+                    while(fread(&jadwal,sizeof(jadwal),1,data)==1){
+                        jadwal.kuota--;
+                    } fwrite(&jadwal,sizeof(jadwal),1,data);
+					fclose(data);
+                }
+                form=fopen("formulir.dat","ab");
+     		    printf("\nNama          : ");fflush(stdin); gets(datap.nama);
+     		    printf("Jenis Kelamin : ");gets(datap.jenisk);
+     		    printf("Umur          : ");gets(datap.umur);
+     		    printf("No. KTP       : ");gets(datap.noktp);
+     	 	    printf("Alamat        : ");gets(datap.alamat);
+        		strcpy(datap.tempv,tempvv); strcpy(datap.jenv,jenvv);
+        		fwrite(&datap, sizeof(datap),1, form);
+       			fclose(form);
+                printf("--Selamat Pendaftaran Vaksinasi Berhasil--\n");
+                printf("\n--Tekan Enter untuk Cetak Bukti Pendaftaran--\n");
+                system("pause"); system("cls"); cover();
+                form=fopen("formulir.dat","rb");
+   				while(fread(&datap,sizeof(datap),1,form)==1){
+        	    	puts("============================");
+            		printf("\nNama          : %s",datap.nama);
+            		printf("\nJenis Kelamin : %s",datap.jenisk);
+            		printf("\nUmur          : %s",datap.umur);
+            		printf("\nNo.KTP        : %s",datap.noktp);
+            		printf("\nAlamat        : %s",datap.alamat);
+            		printf("\nTempat Vaksin : %s",datap.tempv);
+            		printf("\nJenis Vaksin  : %s\n",datap.jenv);
+            		puts("============================");
+    			} fclose(form);
+                puts("--Bukti Pendaftaran Sudah Tercetak--");
+                system("pause");
+                menupasien();
+            }
+        }
+        rewind(data);
+        while(fread(&jadwal,sizeof(jadwal),1,data)==1)
+        {
+            if((strcmp(jadwal.tempat,tempvv)!=0 || strcmp(jadwal.jenisvaksin,jenvv)!=0))
+            {
+                printf("--Maaf Pendaftaran GAGAL--");
+                fclose(data);
+                system("pause");
+                menupasien();
+            }
+        }
+    }
+    else if (strcmp(c,"Y")==0|| strcmp(c,"y")==0){
+        puts("---Maaf anda tidak bisa mendaftar vaksinasi---");
+        system("pause");
+        menupasien();
+    }
+    else {
+        puts("ERROR NGAB!!!");
+        system("pause");
+    }
+}
